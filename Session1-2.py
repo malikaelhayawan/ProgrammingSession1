bb917ed38fc8f7a8c54a48736112d8e6bf6ff662
malikaelhayawan <malikaelhayawan@gmail.com>
2026-01-20 16:59:37 +0100

    My first commit ever

---
+++ Session1-2.py
@@ -0,0 +1,23 @@
+# THIS IS MY FIRST CODE
+# print("hello world")
+print("7+6")
+print(7+6)
+print('hello world')
+# print("and john said 'Hi'")
+print("hello"" ""world")
+# print('"'"'")
+
+print("3 + 4 =" , 3+4 , "and 5 x 6 =" , 5*6)
+print("i have $", 100+200, ".")
+print("i have $", 100+200, ".", sep="")
+# end is used to tell the print what to do after it prints
+print("1", end="")
+print("2", end="")
+print("3", end="!!\n\n")
+print("4", end="\n\n\n")
+print("5")
+print("\"'")
+print(2+3*6/2)
+print(4//2)
+# put two slashes to remove the decimal point
+
